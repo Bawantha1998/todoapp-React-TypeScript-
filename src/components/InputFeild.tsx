687b49485aea74983c1a2089500b3328a95f8c2d
Bawantha1998 <bawanthaprampd918@outlook.com>
2024-01-10 14:11:31 +0530

    fix todo overirde function nd add style

--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -7,23 +7,27 @@ interface Props {
     handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputFeild = ({todo,setTodo,handleAdd}:Props) => {
+const InputFeild : React.FC<Props> = ({todo,setTodo,handleAdd}:Props) => {
 const inputRef = useRef <HTMLInputElement>(null);
 
   return (
-    <form className="input" onSubmit ={(e) => {
-      handleAdd(e)
+    <form className="input"
+     onSubmit ={(e) => {
+      handleAdd(e);
       inputRef.current?.blur();
-    }}>
-        <input type="input"
-        ref = {inputRef}
+    }}
+    >
+        <input 
+        type="text"
+        placeholder='Enter a task'
         value={todo}
+        ref = {inputRef}
         onChange={(e) => setTodo(e.target.value)}
-         placeholder='Enter a task' className='input_box'
+         className="input_box"
          />
         <button className='input_submit' type='submit'>Go</button>
     </form>
-  )
-}
+  );
+};
 
-export default InputFeild
+export default InputFeild;
